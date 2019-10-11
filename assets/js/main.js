befc6debe0163aa23cfdebe8ebe5795eed5aacbf
lpigou <lionelpigou@gmail.com>
2019-10-11 12:36:02 +0200

    random afspraak links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,44 @@
 ---
 ---
+
+var links = [ "https://www.mtc-it4.be/patient/index.html#/appointments/multi/bjNkcWkyQmFRMkpDVTRFNndleFgzQUVXOUM5YmFUWU4wbHNlWHF6MnJNSDdDYUFQdWNjdzZNcm82MU9pYk00Uw==/",
+              "https://www.mtc-it4.be/patient/index.html#/appointments/day/bjNkcWkyQmFRMkpDVTRFNndleFgzTlliK2NyRnpSbkdTQ2dVTjRhbUw3MVpTaVhobExFdVowTnVQUG5vZGM2Tw==/"];
+
+var afspraakLink = function() {
+	// get a random number between 0 and the number of links
+	var randIdx = Math.random() * links.length;
+	// round it, so it can be used as array index
+	randIdx = parseInt(randIdx, 10);
+	// construct the link to be opened
+	var link = links[randIdx];
+
+	return link;
+};
+
+var randomLinks = function(){
+	var knoppen = document.getElementsByClassName("afspraak-knop");
+	//console.log("random");
+
+    for (var i = knoppen.length - 1; i >= 0; i--) {
+    	var knop = knoppen[i];
+    	if(knop){
+    		knop.href = afspraakLink();
+    	}
+    }
+    return;
+};
+
+
 function ready(){
 	var x = document.getElementsByClassName("page-image")[0];
-	if(x == null) return;
-
-	if(x.height > 300){
-		x.style.marginTop = ""+-(x.height-300)/2.0+"px";
+	if(x != null){
+		if(x.height > 300){
+			x.style.marginTop = ""+-(x.height-300)/2.0+"px";
+		}
 	}
+
+	randomLinks();
+	return;
 }
 
 var y = document.getElementsByClassName("page-image")[0];
